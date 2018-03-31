c128e019bff68da5d6ac4eb6b72b6a55054ec6bd
Rui Mao <mr618show@hotmail.com>
2018-03-31 10:30:55 -0700

    Dropping a Face on the Tray

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class CanvasViewController: UIViewController, UIGestureRecognizerDelegate {
 
-    @IBOutlet weak var downArrow: UIImageView!
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
     var trayCenterWhenOpen: CGPoint!
     var trayCenterWhenClosed: CGPoint!
     var newlyCreatedFace: UIImageView!
+    var smileyOriginalCenter: CGPoint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: trayView.frame.size.height/2)
         trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: self.view.frame.height - trayView.frame.size.height/2)
     }
@@ -46,21 +45,19 @@ class CanvasViewController: UIViewController, UIGestureRecognizerDelegate {
                            options: UIViewAnimationOptions.curveEaseIn,
                            animations: {recognizer.view!.center = finalPoint!},
                            completion: nil)
-        } else if recognizer.state == .changed {
-                downArrow.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
         }
-        
     }
     
     @IBAction func onSmileyPanGesture(_ recognizer: UIPanGestureRecognizer) {
         if recognizer.state == .began {
             let imageView = recognizer.view as! UIImageView
+            smileyOriginalCenter = imageView.center
             newlyCreatedFace = UIImageView(image: imageView.image)
             newlyCreatedFace.frame = imageView.frame
             newlyCreatedFace.contentMode = .scaleToFill
             view.addSubview(newlyCreatedFace)
             
-            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(onNewSmileyPangesture(recognizer:)))
             let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(recognizer:)))
             let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(handleSmileyRotation(recognizer:)))
     
@@ -81,40 +78,45 @@ class CanvasViewController: UIViewController, UIGestureRecognizerDelegate {
         
     }
     
-    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
-        print("detect pan")
+    @objc func onNewSmileyPangesture(recognizer: UIPanGestureRecognizer) {
+        var smileyFinalPoint: CGPoint?
         let translation = recognizer.translation(in: self.view)
-        if let view = recognizer.view {
-            view.center = CGPoint(x:view.center.x + translation.x,
-                                  y:view.center.y + translation.y)
+        if recognizer.state == .changed {
+            if let view = recognizer.view {
+                view.center = CGPoint(x:view.center.x + translation.x,
+                                      y:view.center.y + translation.y)
+                recognizer.setTranslation(CGPoint.zero, in: self.view)
+            }
+        } else if recognizer.state == .ended {
+            let velocity = recognizer.velocity(in: self.view)
+            if velocity.y > 0 && trayView.center == trayCenterWhenClosed {
+                smileyFinalPoint = CGPoint(x: smileyOriginalCenter.x, y: smileyOriginalCenter.y + view.frame.height - trayView.frame.height)
+            } else if velocity.y < 0 && trayView.center == trayCenterWhenOpen {
+                smileyFinalPoint = smileyOriginalCenter
+           }
+            UIView.animate(withDuration: 0.35, delay: 0,
+                           usingSpringWithDamping: 0.9,
+                           initialSpringVelocity: 0.5,
+                           options: UIViewAnimationOptions.curveEaseIn,
+                           animations: {
+                            if let finalPoint = smileyFinalPoint{
+                            recognizer.view!.center = finalPoint}
+                            },
+                           completion: {finished in recognizer.view?.removeFromSuperview()})
         }
-        recognizer.setTranslation(CGPoint.zero, in: self.view)
-    
     }
     
     @objc func handlePinch(recognizer: UIPinchGestureRecognizer) {
-        print("detect pinch")
         let view = recognizer.view as! UIImageView
         view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
         recognizer.scale = 1
-        
     }
     
     @objc func handleSmileyRotation(recognizer: UIRotationGestureRecognizer) {
-        print("detect rotation")
         let view = recognizer.view as! UIImageView
         view.transform = view.transform.rotated(by: recognizer.rotation)
         recognizer.rotation = 0
     }
-    
-    @objc func handleArrowRotation(recognizer: UIRotationGestureRecognizer) {
-        print("detect rotation")
-        let view = recognizer.view as! UIImageView
-        view.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
-    }
-
-    
-    
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
